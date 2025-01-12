c8d4b036cc446ba2089d8498b8c20dec993af983
“kiwanacollins” <collinzcalson@gmail.com>
2025-01-12 15:30:27 +0300

    feat: update game outcome display and implement comparison logic for player and computer choices

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
 let chozen = document.getElementById('chozen');
 let computerChoice = document.getElementById('computer-choice')
+let outcome = document.getElementById('outcome')
 
 let humanChoice;
+let choiceComputer
+
 rock.addEventListener('click', () =>{
     humanChoice = 'rock'
     chozen.textContent = `You chose ${humanChoice}`;
     computerChoice.textContent = computerPlay()
+    outcome.textContent = compare(humanChoice, computerChoice)
 }) 
 
 paper.addEventListener('click', () => {
     humanChoice = 'paper'
     chozen.textContent = `You Choze ${humanChoice}`;
     computerChoice.textContent = computerPlay()
+    outcome.textContent = compare(humanChoice, computerChoice)
 })
 
 scissors.addEventListener('click',()=>{
     humanChoice = 'scissors'
     chozen.textContent = `You Choze ${humanChoice}`;
     computerChoice.textContent = computerPlay()
+    outcome.textContent = compare(humanChoice, computerChoice)
 })
 
 
@@ -29,6 +35,41 @@ scissors.addEventListener('click',()=>{
 function computerPlay(){
     let choices = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random() * 3);
-    let choiceComputer = choices[random]
-    return `Computer choze ${choiceComputer}`
+    computerChoice = choices[random]
+    return `Computer choze ${computerChoice}`
+}
+
+function compare(humanChoice, computerChoice){
+    if(humanChoice === 'rock' && computerChoice === 'scissors'){
+           console.log('You win');
+        return 'You win'
+     
+        
+    }else if(humanChoice === 'rock' && computerChoice === 'paper'){
+         console.log('You lose');
+        return 'You lose'
+       
+    }else if(humanChoice === 'paper' && computerChoice === 'rock'){
+           console.log('You win');
+        return 'You win'
+     
+        
+
+    }else if(humanChoice === 'paper' && computerChoice === 'scissors'){
+         console.log('You lose');
+        return 'You lose'
+       
+    }else if(humanChoice === 'scissors' && computerChoice === 'paper'){
+           console.log('You win');
+        return 'You win'
+     
+    }else if(humanChoice === 'scissors' && computerChoice === 'rock'){
+          console.log('You lose');
+        return 'You lose'
+      
+    }else if(humanChoice === computerChoice){
+           console.log('Draw');
+        return 'Draw'
+     
+    }
 }
